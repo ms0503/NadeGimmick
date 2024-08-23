95b73b5653088b59182aba05fb7a73bea4c99373
Sora Tonami <ms0503@outlook.com>
2024-08-24 00:38:51 +0900

    add version field to menu

---
+++ Editor/Constants.cs
@@ -0,0 +1,9 @@
+namespace NadeGimmick.Editor {
+    public static class Constants {
+        public const string NAME = "Nade Gimmick";
+        public const string VERSION = "1.0.1";
+        public const string MENU_ROOT = "Tools/" + NAME;
+        public const string MENU_SETTINGS = MENU_ROOT + "/Settings";
+        public const string MENU_VERSION = MENU_ROOT + "/Version: " + VERSION;
+    }
+}
